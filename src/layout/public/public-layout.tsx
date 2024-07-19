48c15c234b0885f094b8f02576eb6c396c512b34
Csarmasz Milán <csarmasz.milan@gmail.com>
2024-07-19 16:09:43 +0200

    public layout font

--- src/layout/public/public-layout.tsx
+++ src/layout/public/public-layout.tsx
@@ -1,9 +1,10 @@
 import { PublicHeader } from "../../components/public/header/public-header";
 import { Outlet } from 'react-router-dom';
+import './public-layout.css';
 
 export const PublicLayout = () => {
     return (
-        <div style={{ padding: '0 40px 40px 40px' }}>
+        <div className='public-layout-container'>
             <PublicHeader />
             <main>
                 <Outlet />
