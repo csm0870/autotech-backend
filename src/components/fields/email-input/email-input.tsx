507f16a5be8e4a6e5b89801e2dc7e6df444584ad
Csarmasz Milán <csarmasz.milan@gmail.com>
2024-07-19 16:09:13 +0200

    PasswordInput component name file name fix; EmailInput component added

---
+++ src/components/fields/email-input/email-input.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import './email-input.css';
+
+type EmailInputProps = {
+    defaultValue?: string;
+    placeholder?: string;
+    error?: string;
+    required?: boolean;
+    disabled?: boolean;
+    onChange?: (value: string) => void;
+}
+
+export const EmailInput = ({ defaultValue, placeholder, error, onChange, required, disabled }: EmailInputProps) => {
+
+    const [value, setValue] = useState(defaultValue ?? '');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+        if(onChange !== undefined) onChange(e.target.value);
+    };
+
+    return (
+        <>
+            <input
+                className="email-input"
+                type="email"
+                value={value}
+                placeholder={required ? `*${placeholder}` : placeholder}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)}
+                required={required}
+                disabled={disabled}
+            />
+            {
+                error !== undefined && (
+                    <div className="email-input-control-error">
+                        {error}
+                    </div>
+                )
+            }
+        </>
+    );
+};
